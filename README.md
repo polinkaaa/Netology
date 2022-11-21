6483b4d6d71ddefb95b1cf90b9a840c81e2e91c2
polinkaaa <polina_kond@mail.ru>
2022-11-22 00:57:19 +0300

    add changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Netology
+# Netology – Микролернинг
+
+* [Демо личного кабинета пользователя](https://polinkaaa.github.io/Netology/libs/personal_account.html)
+
+* [Демо ленты](https://polinkaaa.github.io/Netology/libs/my_course.html)
--- js/main.js
+++ js/main.js
@@ -4,3 +4,29 @@ $('.slider-one').slick({
     vertical: true,
     verticalSwiping: true
 });
+
+
+
+
+function RangAmongFriends() {
+    var a = document.getElementById("rang-in-Russia")
+    var y = document.getElementById("rang-among-friends")
+    a.style.display ='none'
+    y.style.display = 'block'
+};
+function RangInRussia() {
+    var a = document.getElementById("rang-in-Russia")
+    var y = document.getElementById("rang-among-friends")
+    a.style.display ='flex'
+    y.style.display = 'block'
+};
+
+
+function openModal() {
+    if (document.getElementById("modal").style.display == "block"){
+        document.getElementById("modal").style.display = "none"
+        document.getElementById("modal").style.top = "-400px"}
+    else {
+        document.getElementById("modal").style.display = "block"
+        document.getElementById("modal").style.top = "70px"}
+}
